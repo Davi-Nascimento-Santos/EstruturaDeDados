9a3b9b7bcc41572054f9366ed7ba0f8aa55ed93c
Davi Nascimento <davinascimento19072001@gmail.com>
2023-03-14 18:14:52 -0300

    subir para o GitHub

---
+++ Atividades/Atividade07/interfaceTeste.py
@@ -0,0 +1,24 @@
+import tkinter
+
+app = tkinter.Tk()
+app.title("Gráfico TOP")
+app.geometry("500x300")
+app.configure(background="#dde")
+app.mainloop()
+
+
+
+
+
+"""import tkinter as tk
+
+root = tk.Tk()
+
+label = tk.Label(root, text="Olá, mundo!")
+label.pack()
+
+button = tk.Button(root, text="Clique aqui!")
+button.pack()
+
+root.mainloop()
+"""
